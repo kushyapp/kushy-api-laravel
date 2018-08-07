0969e0181e0895da8c95f863df44a4b5d8374767
Ryosuke <ryosuke.san.hana@gmail.com>
2018-08-07 16:59:51 -0700

    :flashing_light: Removed debugging from factory

--- database/factories/UserActivityFactory.php
+++ database/factories/UserActivityFactory.php
@@ -9,7 +9,6 @@ use Faker\Generator as Faker;
 
 $factory->define(KushyApi\UserActivity::class, function (Faker $faker) {
     $userId = factory(KushyApi\User::class)->create()->id;
-    echo $userId;
     return [
         'id' => $faker->uuid,
         'user_id' => $userId,
