17dd487b633dbe770b0cb827a8eb34cd50fe7fe5
Ryosuke <ryosuke.san.hana@gmail.com>
2018-08-07 14:17:11 -0700

    :bug: Dupe username faker error fix using random num

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,7 +16,7 @@ use Faker\Generator as Faker;
 $factory->define(KushyApi\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'username' => $faker->unique()->userName,
+        'username' => $faker->unique()->userName . str_random(2),
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
