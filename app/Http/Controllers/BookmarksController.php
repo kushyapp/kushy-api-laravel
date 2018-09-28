f2d00aa6a2c4539d490839cdca672fb0f4bb313e
Ryosuke <ryosuke.san.hana@gmail.com>
2018-09-28 16:25:36 -0700

    :hammer: Bookmarks no longer extends API base class

--- app/Http/Controllers/BookmarksController.php
+++ app/Http/Controllers/BookmarksController.php
@@ -3,59 +3,113 @@
 namespace KushyApi\Http\Controllers;
 
 use Illuminate\Http\Request;
-use KushyApi\Http\Controllers\ApiController;
+use Spatie\QueryBuilder\QueryBuilder;
+use KushyApi\Http\Controllers\Controller;
 use KushyApi\Http\Requests\StoreBookmarks;
+use KushyApi\Http\Requests\DeleteBookmark;
 use KushyApi\Http\Resources\Bookmarks as BookmarksResource;
 use KushyApi\Http\Resources\BookmarksCollection;
 use KushyApi\Bookmarks;
 
-class BookmarksController extends ApiController
+class BookmarksController extends Controller
 {
-    /**
-     * Section name to be displayed when deleting items
-     *
-     * @var string
-     */
-    protected $section = 'cart';
-
-    /**
-     * The model to use when querying/deleting
-     *
-     * @var [type]
-     */
-    protected $model = Bookmarks::class;
-
-    /**
-     * Relationships to allow users to include in queries
-     *
-     * @var array
-     */
-    protected $includes = [
-        'user', 
-        'post', 
-    ];
-
-    /**
-     * Allowed filters for users 
-     *
-     * @var array
-     */
-    protected $filters = [
-        'post_id',
-        'user_id'
-    ];
-
-    /**
-     * API Resource collection
-     *
-     * @var [type]
-     */
-    protected $resourceCollection = BookmarksCollection::class;
     
+    public function __construct() 
+    {
+        $this->middleware('auth:api', ['except' => ['index', 'show']]);
+    }
+
     /**
-     * API Resource
+     * Display a listing of the resource.
      *
-     * @var [type]
+     * @return \Illuminate\Http\Response
      */
-    protected $resource = BookmarksResource::class;
+    public function index(Request $request)
+    {
+        /**
+         * We use Spatie's Query Builder package to handle
+         * filtering, sorting, and includes
+         */
+        $categories = QueryBuilder::for(Bookmarks::class)
+            ->allowedFilters([
+                'post_id',
+                'user_id'
+            ])
+            ->allowedIncludes([
+                'user',
+                'post',
+                'activity'
+            ])
+            ->get();
+
+        return (new BookmarksCollection($categories))
+            ->response()
+            ->setStatusCode(201);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * We don't type check the request, it's checked by extended class
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreBookmarks $request)
+    {
+        $category = Bookmarks::create($request->all());
+
+        return (new BookmarksResource($category))
+            ->response()
+            ->setStatusCode(201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $categories = Bookmarks::findOrFail($id);
+
+        return (new BookmarksResource($categories))
+            ->response()
+            ->setStatusCode(201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // Grab the inventory item so we can update it
+        $category = Bookmarks::findOrFail($id);
+
+        $category->fill($request->all());
+        
+        return (new BookmarksResource($category))
+            ->response()
+            ->setStatusCode(201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DeleteBookmark $request, $id)
+    {
+        Bookmarks::destroy($id);
+
+        return response()->json([
+            'code' => true,
+            'response' => "Successfully deleted the bookmark."
+        ]);
+    }
 }
