bdb54dd66860ca132b785c7f10582108dd393c72
Ryosuke <ryosuke.san.hana@gmail.com>
2018-11-20 13:30:05 -0800

    :wrench: Check for Heroku env for DB

--- config/database.php
+++ config/database.php
@@ -1,5 +1,23 @@
 <?php
 
+
+$DB_HOST = env('DB_HOST', '127.0.0.1'),
+$DB_PORT = env('DB_PORT', '3306'),
+$DB_DATABASE = env('DB_DATABASE', 'forge'),
+$DB_USERNAME = env('DB_USERNAME', 'forge'),
+$DB_PASSWORD = env('DB_PASSWORD', ''),
+
+if(env('APP_ENV') == 'heroku')
+{
+    $DATABASE_URL = parse_url(getenv("DATABASE_URL"));
+
+    $DB_HOST = $DATABASE_URL["host"],
+    $DB_PORT = $DATABASE_URL["port"],
+    $DB_DATABASE = ltrim($DATABASE_URL["path"], "/"),
+    $DB_USERNAME = $DATABASE_URL["user"],
+    $DB_PASSWORD = $DATABASE_URL["pass"],
+}
+
 return [
 
     /*
@@ -41,11 +59,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DB_HOST,
+            'port' => $DB_PORT,
+            'database' => $DB_DATABASE,
+            'username' => $DB_USERNAME,
+            'password' => $DB_PASSWORD,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
