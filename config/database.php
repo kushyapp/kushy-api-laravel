99360c439865fcf687c68c222d3bd0c9417486a5
Ryosuke <ryosuke.san.hana@gmail.com>
2018-11-20 13:47:07 -0800

    :wrench: Swapped MySQL and PQSQL heroku config

--- config/database.php
+++ config/database.php
@@ -59,11 +59,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => $DB_HOST,
-            'port' => $DB_PORT,
-            'database' => $DB_DATABASE,
-            'username' => $DB_USERNAME,
-            'password' => $DB_PASSWORD,
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -74,11 +74,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DB_HOST,
+            'port' => $DB_PORT,
+            'database' => $DB_DATABASE,
+            'username' => $DB_USERNAME,
+            'password' => $DB_PASSWORD,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
